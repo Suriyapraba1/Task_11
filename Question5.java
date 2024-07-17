5dec3ac8b6be3408cc01a8cf1524aa1a2933dd6b
Suriyapraba1 <167987270+Suriyapraba1@users.noreply.github.com>
2024-07-17 23:08:22 +0530

    Add files via upload

---
+++ Question5.java
@@ -0,0 +1,18 @@
+package Task_11;
+public class Question5
+{
+    public static void main(String[] args)
+    {
+        int[] numbers = {1, 2, 3, 4, 5};
+
+        // Trying to access an index that is out of bounds
+        int index = 10;
+        try
+        {
+            int value = numbers[index];
+            System.out.println("Value at index " + index + ": " + value);
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Error: Array index out of bounds.");
+        }
+    }
+}
